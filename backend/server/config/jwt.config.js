3d18769bf559ded78e6211634123a0a932b04d85
jehala <jenniferehala@github.com>
2022-05-03 13:40:38 -0400

    commit

--- backend/server/config/jwt.config.js
+++ backend/server/config/jwt.config.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports.authenticate = (req, res, next) => {
     jwt.verify(req.cookies.usertoken, process.env.SECRET_KEY, (err, payload) => {
+        console.log(err)
         if (err) {
             // res.status(401).json({ verified: false });
             res.redirect("/")
